7af344e626a7d354521f8b13796aff6bbdb0f4b8
Brent Berie <161008695+brentberry@users.noreply.github.com>
2024-02-23 10:02:07 +0200

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,32 @@
+void setClockScale(VOLTS __x)
+{
+  uint8_t __tmp = _BV(CLKPCE); /*[1]*/
+  __asm__ __volatile__ (
+    "in __tmp_reg__,__SREG__" "\n\t"
+    "cli" "\n\t"
+    "sts %1, %0" "\n\t"
+    "sts %1, %2" "\n\t"
+    "out __SREG__, __tmp_reg__"
+    : /* no outputs */
+    : "d" (__tmp),
+    "M" (_SFR_MEM_ADDR(CLKPR)),
+    "d" (__x)
+    : "r0"
+  ); /*[2]*/
+}
+
+  #if defined(ENABLE_3V3FIX)
+      // Stop serial if running
+    #if !defined(enable_serial) && defined(ENABLE_UPDATER)
+      ClockedSerial.end();
+    #endif
+      // Set clock speed        
+      clock = CS_16MHZ;
+      setClockScale(newVoltage); /*[2]*/
+      // Restart serial
+    #if !defined(enable_serial) && defined(ENABLE_UPDATER)
+      ClockedSerial.begin(UPD_BAUD);
+    #endif
+  #else
+      clock = CS_16MHZ;
+  #endif
